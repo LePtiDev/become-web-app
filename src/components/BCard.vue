b98ab6bd94d49aa0480dc9663ee8aede10037fa6
Quentin Guerrier <75quentinguerrier@gmail.com>
2022-05-20 15:48:44 +0200

    fix conflics

--- src/components/BCard.vue
+++ src/components/BCard.vue
@@ -4,11 +4,11 @@
       <h2 class="text-lg font-regular text-gray-900">{{ title }}</h2>
       <slot name="header-right" />
     </div>
-    <div class="px-4">
+    <div class="px-4 py-4">
       <slot />
     </div>
     <!-- Remplacer par une div avec des boutons -->
-    <slot name="footer" class="px-4 py-4 sm:px-6" />
+    <slot name="footer" class="px-4 py-4" />
   </div>
 </template>
 <script lang="ts">
