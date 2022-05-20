a4e8e98e37b80178ac4bc7788e63bd0829459757
Antoine Puech <antoine.puech@edu.devinci.fr>
2022-05-20 15:44:55 +0200

    dynamise card component

--- src/components/BCard.vue
+++ src/components/BCard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="bg-white overflow-hidden shadow rounded-lg">
-    <div v-if="title" class="px-4 py-5 sm:px-6 flex justify-between">
-      <h2 class="text-lg font-regular text-gray-900">{{ title }}</h2>
+  <div class="bg-white overflow-hidden shadow rounded-lg w-72">
+    <img :src="image" alt="node js and mongo DB" class="w-full"/>
+    <div v-if="title" class="px-4 pt-5 sm:px-6 justify-between">
+      <h2 class="text-lg font-bold text-gray-900">{{ title }}</h2>
+      <p class="pb-3">{{ teacher }}</p>
       <slot name="header-right" />
     </div>
-    <div class="px-4 py-4">
-      <slot />
+    <div class="px-4">
+
     </div>
     <!-- Remplacer par une div avec des boutons -->
     <slot name="footer" class="px-4 py-4 sm:px-6" />
@@ -16,6 +18,8 @@ import Vue from "vue";
 export default Vue.extend({
   props: {
     title: String,
+    teacher: String,
+    image: String,
   },
 });
 </script>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -8,14 +8,8 @@
       </template>
     </Bheader>
     <main>
-      <div class="px-24 flex justify-between">
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
-        <BCard title="Node.js Socket.io" />
+      <div class="px-24 flex flex-wrap justify-between">
+        <BCard class="mb-6" v-for="course in courses" :key="course.title" :title="course.title" :teacher="course.teacher" :image="course.image"/>
       </div>
     </main>
   </div>
@@ -26,6 +20,26 @@ import BCard from "./../components/BCard.vue";
 import Bbutton from "./../components/Bbutton.vue";
 import Vue from "vue";
 export default Vue.extend({
-
+  data() {
+    return {
+      courses: [
+        {
+          title: "Node.js Nest",
+          teacher: "Alexandre ZERAH",
+          image: "https://courses.nestjs.com/img/nest-courses-og.png",
+        },
+        {
+          title: "Laravel",
+          teacher: "Pierre GRIMAUD",
+          image: "https://oursblanc.tech/wp-content/uploads/2020/12/logolaravel-e1607524036663.png",
+        },
+        {
+          title: "Initiation Vue.js V3",
+          teacher: "Alexis BOUGY",
+          image: "https://vuejsdevelopers.com/images/posts/vue_3_tutorial.png",
+        },
+      ],
+    };
+  },
 });
 </script>
